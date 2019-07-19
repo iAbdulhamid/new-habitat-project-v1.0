6e8a9038d6a3d09519eb07036b95ecb8db5ebddc
iAbdulhamid <i_abdulhamid@outlook.com>
2019-07-19 20:16:26 +0200

    initial-commit-setting-homepage

---
+++ src/components/homepage/homepage.component.jsx
@@ -0,0 +1,46 @@
+import React from 'react';
+import './homepage.component.scss'
+
+
+const HomePage = () => (
+    <div className="homepage">
+        <div className="directory-menu">
+            <div className="menu-item">
+                <div className="content">
+                    <h1 className="title">Refrigerators</h1>
+                    <span className="subtitle">ٍShop Now</span>
+                </div>
+            </div>
+
+            <div className="menu-item">
+                <div className="content">
+                    <h1 className="title">Refrigerators</h1>
+                    <span className="subtitle">ٍShop Now</span>
+                </div>
+            </div>
+
+            <div className="menu-item">
+                <div className="content">
+                    <h1 className="title">Refrigerators</h1>
+                    <span className="subtitle">ٍShop Now</span>
+                </div>
+            </div>
+            <div className="menu-item">
+                <div className="content">
+                    <h1 className="title">Refrigerators</h1>
+                    <span className="subtitle">ٍShop Now</span>
+                </div>
+            </div>
+            <div className="menu-item">
+                <div className="content">
+                    <h1 className="title">Refrigerators</h1>
+                    <span className="subtitle">ٍShop Now</span>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    
+);
+
+export default HomePage;
