b8a43d4a4d4e93ffe78d0603d80ceac766158958
iAbdulhamid <i_abdulhamid@outlook.com>
2019-07-24 16:55:57 +0200

    issue fixed: redirect the user to homepage after login

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import { Switch, Route } from 'react-router-dom';
+import { Switch, Route, Redirect } from 'react-router-dom';
 
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop-page/shop-page.component';
@@ -52,7 +52,8 @@ class App extends React.Component {
           <Route exact path = "/" component = { HomePage } />
           {/* <Route path = "/Refrigerators" component = { RefrigeratorsPage } /> */}
           <Route path = "/shop" component = {ShopPage} />        
-          <Route path = "/signIn" component = {SignInAndUpPage} />
+          <Route exact path = "/signIn" 
+          render = { () => this.props.currentUser ? (<Redirect to = '/' />) : (<SignInAndUpPage />) } />
         </Switch>
   
       </div>
@@ -61,8 +62,12 @@ class App extends React.Component {
   
 }
 
+const mapStateToProps = ({user}) => ({
+  currentUser: user.currentUser
+});
+
 const mapDispatchToProps = dispatch => ({
   setCurrentUser: user => dispatch(setCurrentUser(user))
 });
 
-export default connect(null, mapDispatchToProps)(App);
+export default connect(mapStateToProps, mapDispatchToProps)(App);
